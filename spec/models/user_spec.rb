f70176772f226f0f8e223f984233f26daa6a5f8c
HansRCode <12411150+HansRCode@users.noreply.github.com>
2021-06-18 16:47:56 -0400

    user model changed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
   # Association test
   # ensure User model has a 1:m relationship with the Todo model
   it { should have_many(:todos) }
@@ -10,5 +9,4 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password_digest) }
-  
 end
