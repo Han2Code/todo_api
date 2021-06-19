2621cd8801ff03d1fa15ca17214fb275f1f9b8d4
HansRCode <12411150+HansRCode@users.noreply.github.com>
2021-06-19 00:19:31 -0400

    Serializers

--- db/schema.rb
+++ db/schema.rb
@@ -10,30 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_618_204_013) do
-  create_table 'items', force: :cascade do |t|
-    t.string 'name'
-    t.boolean 'done'
-    t.integer 'todo_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['todo_id'], name: 'index_items_on_todo_id'
+ActiveRecord::Schema.define(version: 2021_06_18_204013) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.boolean "done"
+    t.integer "todo_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["todo_id"], name: "index_items_on_todo_id"
   end
 
-  create_table 'todos', force: :cascade do |t|
-    t.string 'title'
-    t.string 'created_by'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "todos", force: :cascade do |t|
+    t.string "title"
+    t.string "created_by"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.string 'password_digest'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key 'items', 'todos'
+  add_foreign_key "items", "todos"
 end
